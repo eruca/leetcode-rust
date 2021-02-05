0f39e96a8ac7c8eb0b83dbd00f3cff0325fe57ef
eruca <nickwill1984@126.com>
2021-02-05 22:05:42 +0800

    first commit

---
+++ last-word-count/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn length_of_last_word(s: String) -> i32 {
+    match s.split_ascii_whitespace().rev().next() {
+        Some(word) => word.len() as i32,
+        None => 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_work() {
+        assert_eq!(length_of_last_word("Hello World".to_string()), 5);
+        assert_eq!(length_of_last_word(" ".to_string()), 0);
+    }
+}
